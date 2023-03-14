82bac7bb6ae233bd92116f5fe3b9f9533def370a
douglass-magalhaes <dm.developer@outlook.com>
2023-03-14 10:12:22 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleano
+
+//conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);
+console.log(numero === numeroString);
+
+// conversao explicita
+
+console.log(numero + parseInt(numeroString));
